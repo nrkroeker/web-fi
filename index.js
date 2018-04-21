01ceaca7e73c53d7b24c04b78caf4fe4aaf7479b
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-04-21 15:04:07 -0400

    Change port for web access

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 app.listen(port);
 app.set('view engine', 'pug')
 app.set('views', './public/views')
