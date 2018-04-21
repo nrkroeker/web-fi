a472bc72dd1d85ff379891be4ed8807ae8c1d6dc
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-04-20 22:43:00 -0400

    Testing server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HEY!');
+  res.send('HEYYYYOOOOOOOO!');
 });
 app.listen(3000, () => console.log('Server running on port 3000'));
