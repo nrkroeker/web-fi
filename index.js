0ac89fba63456f7cf905e60899b97c4b8a0ae8be
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-04-21 15:23:23 -0400

    Fixing port cause that's not how it works apparently

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = 3000;
 app.listen(port);
 app.set('view engine', 'pug')
 app.set('views', './public/views')
